f966768be7fad6deba967be2e7ad8abac96c6641
German Kostiakov <KostiakovG@gmail.com>
2025-06-06 17:27:54 +0300

    removed test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ function App() {
   return (
     <div>
       <h1 className="text-5xl">Portfolio Website</h1>
-      <div className="bg-red-500 p-8 text-white text-4xl">
-        TAILWIND TEST - This should be red background, white text, large
-      </div>
     </div>
   );
 }
