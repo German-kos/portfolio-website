0a0bc01b915e7bfe4c7be2a7b113c2dc5ef85a4d
German Kostiakov <KostiakovG@gmail.com>
2025-06-08 15:14:58 +0300

    fixed glass texture

--- src/sections/hero/components/static-background.tsx
+++ src/sections/hero/components/static-background.tsx
@@ -1,5 +1,8 @@
-export const StaticBackground = () => {
+const StaticBackground = () => {
   return (
-    <div className="z-[-1] absolute bg-black/25 shadow-2xl shadow-black/50 backdrop-blur-2xl border-purple-200/15 min-w-160 min-h-600 rotate-18" />
+    <div className="z-[-1] absolute inset-0 flex justify-center items-center">
+      <div className="bg-black/25 shadow-2xl shadow-black/50 backdrop-blur-xs border border-purple-200/10 min-w-160 min-h-600 rotate-18" />
+    </div>
   );
 };
+export default StaticBackground;
