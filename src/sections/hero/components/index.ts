1845286749f13bdd77735b4862421dab868c5095
German <kostiakovg@gmail.com>
2025-06-11 22:16:16 +0300

    updated exports

--- src/sections/hero/components/index.ts
+++ src/sections/hero/components/index.ts
@@ -1,3 +1,4 @@
 export { default as BreathingBackground } from "./breathing-background";
 export { default as StaticBackground } from "./static-background";
 export { default as SymbolsBackground } from "./symbols-background";
+export { default as ResponsiveBackground } from "./responsive-background";
