acfee03ee9480768cfd776ec526534611c4c3d67
German Kostiakov <KostiakovG@gmail.com>
2025-06-12 07:52:20 +0300

    exports updated

--- src/sections/hero/components/index.ts
+++ src/sections/hero/components/index.ts
@@ -2,3 +2,4 @@ export { default as BreathingBackground } from "./breathing-background";
 export { default as StaticBackground } from "./static-background";
 export { default as SymbolsBackground } from "./symbols-background";
 export { default as ResponsiveBackground } from "./responsive-background";
+export { default as StaticGradientBackground } from "./static-gradient-background";
