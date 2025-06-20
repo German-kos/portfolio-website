d39b252fa0e65e071c4ccbd75d8b2bb7e89edf79
German Kostiakov <KostiakovG@gmail.com>
2025-06-20 19:04:44 +0300

    placeholder skills section

---
+++ src/sections/skills/skills.tsx
@@ -0,0 +1,12 @@
+import { ShapeDivider } from "../../components";
+
+const Skills = () => {
+  return (
+    <section className="relative flex flex-col justify-center items-center bg-gradient-to-b from-purple-300 to-red-300 p-8 min-h-screen text-black">
+      my skills here
+      <ShapeDivider flip={true} fill="#fca5a5" />
+    </section>
+  );
+};
+
+export default Skills;
