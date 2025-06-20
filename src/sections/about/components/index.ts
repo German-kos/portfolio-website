ec47ef0ba783c19842a980b855861db07631ca37
German Kostiakov <KostiakovG@gmail.com>
2025-06-20 23:57:37 +0300

    updated exports

--- src/sections/about/components/index.ts
+++ src/sections/about/components/index.ts
@@ -1,2 +1,3 @@
 export { default as GlassMacWindow } from "./glass-mac-window";
 export { default as MacTerminalWindow } from "./mac-terminal-window";
+export { default as BlobProfileImage } from "./blob-profile-image";
