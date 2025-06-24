27ca92f34f3b8b09d134f88a40f87147032cd994
German <kostiakovg@gmail.com>
2025-06-24 20:02:24 +0300

    updated exports

--- src/sections/contact/components/index.ts
+++ src/sections/contact/components/index.ts
@@ -1,1 +1,4 @@
 export { default as GlassMacWindow } from "./glass-mac-window";
+export { default as ContactForm } from "./contact-form";
+export { default as ContactDivider } from "./contact-divider";
+export { default as ContactAlternative } from "./contact-alternative";
