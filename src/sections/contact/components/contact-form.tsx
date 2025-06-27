daae5ab1e6b39af2bfc0c5a8016a552e15caeb15
German <kostiakovg@gmail.com>
2025-06-27 21:08:51 +0300

    updated import

--- src/sections/contact/components/contact-form.tsx
+++ src/sections/contact/components/contact-form.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import FormField from "./form-field";
+import { FormField } from ".";
 
 interface FormData {
   name: string;
