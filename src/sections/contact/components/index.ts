95d056b50bc0468bf9a86d6eed67197e094636e8
German <kostiakovg@gmail.com>
2025-06-27 22:59:53 +0300

    updated exports

--- src/sections/contact/components/index.ts
+++ src/sections/contact/components/index.ts
@@ -3,3 +3,4 @@ export { default as ContactForm } from "./contact-form";
 export { default as ContactDivider } from "./contact-divider";
 export { default as ContactAlternative } from "./contact-alternative";
 export { default as FormField } from "./form-field";
+export { FormSuccessMessage, FormErrorMessage } from "./form-status-messages";
