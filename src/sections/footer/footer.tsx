ecaeaeee6bb4e3c9e0f6566ceb3d38ff34b3d6ed
German <kostiakovg@gmail.com>
2025-06-28 19:36:37 +0300

    Added a commented privacy statement for later, and a divider between the top and bottom part of the footer.

--- src/sections/footer/footer.tsx
+++ src/sections/footer/footer.tsx
@@ -1,8 +1,11 @@
 import { ShapeDivider } from "../../components";
 
 const Footer = () => {
+  // TODO: Make this a tooltip instead of a link or modal since it's short.
+  // const privacyPolicy =
+  //   "Contact information submitted through this form is used solely to respond to your inquiry and is not shared with third parties.";
   return (
-    <section className="bg-gray-900 flex items-center justify-center relative pt-32">
+    <section className="bg-gray-900 flex pb-8 items-center justify-center relative pt-32">
       <div className="flex flex-col items-center justify-center gap-4">
         <span className="text-gray-400 italic flex flex-row gap-6 text-2xl ">
           <p className="terminal-font ">[</p>
@@ -10,7 +13,7 @@ const Footer = () => {
           <p className="terminal-font"> full-stack developer</p>
           <p className="terminal-font">]</p>
         </span>
-        <span className="text-gray-400 text-sm flex flex-row gap-12">
+        <span className="text-gray-400 border-t pt-2 border-gray-300/30   text-sm flex flex-row gap-12">
           <div className="flex flex-col items-center justify-center">
             <p className="terminal-font">contact</p>
             <p className="terminal-font">kostiakovg@gmail.com</p>
