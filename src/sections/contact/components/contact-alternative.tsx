074d530a11c0990a2f8c3c274707269ab5cb2997
German <kostiakovg@gmail.com>
2025-06-28 17:19:55 +0300

    Button animations have been changed and now match the hero section's quick button animation.

--- src/sections/contact/components/contact-alternative.tsx
+++ src/sections/contact/components/contact-alternative.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import { contactData } from "../../../data";
 
 const ContactAlternative = () => {
@@ -5,10 +6,17 @@ const ContactAlternative = () => {
     <div className="flex flex-col gap-6 items-center">
       <div className="flex md:flex-row flex-col justify-center items-center gap-4 ">
         {contactData.map((item, index) => (
-          <button
+          <motion.button
             key={index}
             onClick={item.action}
-            className={`flex items-center gap-3 justify-center  ${item.bgColor} ${item.hoverBgColor} shadow-lg hover:shadow-xl hover:border-white hover:cursor-pointer backdrop-blur-sm px-6 py-4 border border-white/50 rounded-xl w-[180px] ${item.textColor} hover:scale-105 transition-all duration-300`}
+            className={`flex items-center justify-center gap-3 ${item.bgColor} ${item.hoverBgColor} shadow-lg hover:shadow-xl hover:border-white hover:cursor-pointer backdrop-blur-sm px-6 py-4 border border-white/50 rounded-xl w-[180px] ${item.textColor} transition-colors`}
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+            transition={{
+              type: "spring",
+              stiffness: 400,
+              damping: 30,
+            }}
           >
             <svg
               className={`w-8 h-8`}
@@ -20,7 +28,7 @@ const ContactAlternative = () => {
               <path d={item.path} />
             </svg>
             <span className="font-medium">{item.text}</span>
-          </button>
+          </motion.button>
         ))}
       </div>
       <p className="text-gray-600 font-medium">Email: kostiakovg@gmail.com</p>
