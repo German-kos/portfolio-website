a8387775fd5e4852edb6d86c85fe9431bbd8067f
German <kostiakovg@gmail.com>
2025-06-28 18:02:55 +0300

    skeleton footer

---
+++ src/sections/footer/footer.tsx
@@ -0,0 +1,35 @@
+import { ShapeDivider } from "../../components";
+
+const Footer = () => {
+  return (
+    <section className="bg-gray-900 flex items-center justify-center relative pt-32">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <span className="text-gray-400 italic flex flex-row gap-6 text-2xl ">
+          <p className="terminal-font ">[</p>
+          <p className="terminal-font"> german,</p>
+          <p className="terminal-font"> full-stack developer</p>
+          <p className="terminal-font">]</p>
+        </span>
+        <span className="text-gray-400 text-sm flex flex-row gap-12">
+          <div className="flex flex-col items-center justify-center">
+            <p className="terminal-font">contact</p>
+            <p className="terminal-font">kostiakovg@gmail.com</p>
+          </div>
+          <div className="flex flex-row gap-4 items-center text-gray-400 justify-center">
+            <p className="terminal-font">icon</p>
+            <p className="terminal-font">icon</p>
+            <p className="terminal-font">icon</p>
+          </div>
+          <div className="flex items-center text-gray-400 justify-center">
+            <p className="terminal-font">
+              © {new Date().getFullYear()} German Kostiakov
+            </p>
+          </div>
+        </span>
+      </div>
+      <ShapeDivider fill="#d8b4fe" flip={true} />
+    </section>
+  );
+};
+
+export default Footer;
